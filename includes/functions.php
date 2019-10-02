3559c07aaa6fe445001c6085ce947045b4efa70f
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-02 23:31:13 +0000

    test17

--- includes/functions.php
+++ includes/functions.php
@@ -84,7 +84,7 @@ $response = json_decode($output, true);
 curl_close($handle);
 /*dont change
 */
-echo '<h2>' . $response["title"] . '</h2>'; //returns image
+echo '<h2>' . $response["Title"] . '</h2>'; //returns image
 }
 
 /*function getMovieTitle(){
