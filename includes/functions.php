cdba95a471e0bbfee7f66c7233c164e346abc23a
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-02 23:22:16 +0000

    test14

--- includes/functions.php
+++ includes/functions.php
@@ -68,7 +68,7 @@ function page_content()
 
 //Gets the movie poster
 function getComicImage(){
-$url = 'http://www.omdbapi.com/?i=tt3896198';
+$url = 'http://www.omdbapi.com/?i=tt3896198&apikey=b1892baf';
 /**dont change
 */
 $handle = curl_init();
