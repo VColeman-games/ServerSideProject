c66fc97d70abd7b26ba56eaf292ccfdae22d3363
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-02 23:14:18 +0000

    test11

--- includes/functions.php
+++ includes/functions.php
@@ -58,11 +58,12 @@ function page_title()
 function page_content()
 {
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-    $path = getcwd() . '/' . config('content_path') . '/' . $page . '.phtml';
+    $path = getcwd() . '/' . config('content_path') . '/' . $page . '.php';
     if (! file_exists($path)) {
-        $path = getcwd() . '/' . config('content_path') . '/404.phtml';
+        $path = getcwd() . '/' . config('content_path') . '/404.php';
     }
-    echo file_get_contents($path);
+    //echo file_get_contents($path);
+    require config('config_path'). $path;
 }
 
 //Gets the movie poster
