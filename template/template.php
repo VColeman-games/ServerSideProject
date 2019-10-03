46f0364e19f59e4c55efa16b2015f50f613cc401
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-03 01:12:03 +0000

    test28

--- template/template.php
+++ template/template.php
@@ -9,14 +9,14 @@
 <div class="wrap">
 
     <header>
-        <h1><?php site_name(); ?></h1>
+        //<h1><?php site_name(); ?></h1>
         <nav class="menu">
             <?php nav_menu(); ?>
         </nav>
     </header>
 
     <article>
-        <h2><?php page_title(); ?></h2>
+        //<h2><?php page_title(); ?></h2>
         <?php page_content(); ?>
     </article>
 
