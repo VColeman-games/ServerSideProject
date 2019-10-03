7677946e80e86672a23225c6fa2474d6be768ab6
cjmarguin <marguin_claudia@columbusstate.edu>
2019-10-03 04:01:43 +0000

    kermit pic and apostrophes

--- content/about-us.php
+++ content/about-us.php
@@ -1,32 +1,32 @@
-<p> Howdy there! We're the girl gang, and we'd like to formally invite you to our little server-side project website!
-here's a little about each of us!</p>
+<p> Howdy there! Were the girl gang, and we&#39;d like to formally invite you to our little server-side project website!
+here&#39;s a little about each of us!</p>
 
 
 
 <h1>Valencia Coleman</h1>
 
-<p> Valencia is a game's track queeeeen! Always repin' CSU is the best of ways, whether it's through
+<p> Valencia is a games track queeeeen! Always repin CSU is the best of ways, whether it&#39;s through
 her work effort or her extracurriculars in the Comp Sci department. I could absolutely never! Valencia is in the girl gang. We stan Valencia. </p>
 
 <br>
 
 <h2>Sharon Deloach </h2>
 
-<p>Sharon is a style ICONNNNN! Always rockin' chokers and clothes that I absolutely wish I had the ability to pull-off. On top of that
+<p>Sharon is a style ICONNNNN! Always rockin chokers and clothes that I absolutely wish I had the ability to pull-off. On top of that
 Sharon is a coding queen! Sharon is in the girl gang. We stan Sharon. </p>
 
 <br>
 
 <h2> Mary Harrell </h2>
 
-<p>Mary is a music feastival fanatic! Of the girl gang, I've known Mary the longest (since Data Structures many moons ago).
-We (attempt) to work in the Shannon Hall Math/Literature tutor lab as tutors together on. (We're not the best at algebra, but we try and that's what really matters). Mary is in the girl gang. We stan Mary. </p>
+<p>Mary is a music feastival fanatic! Of the girl gang, I&#39;ve known Mary the longest (since Data Structures many moons ago).
+We (attempt) to work in the Shannon Hall Math/Literature tutor lab as tutors together on. (Were not the best at algebra, but we try and that&#39;s what really matters). Mary is in the girl gang. We stan Mary. </p>
 
 <br>
 
 <h2> Claudia Marguin </h2>
 
-<p> Lol. This is me. I love dogs and Minecraft. While I'm also passionate about computer science like the three other amazing
-gal in this group, I'm also a passionate member of greek life here at CSU. I'm in the girl gang. We stan me?</p>
+<p> Lol. This is me. I love dogs and Minecraft. While I&#39;m also passionate about computer science like the three other amazing
+gal in this group, I&#39;m also a passionate member of greek life here at CSU. I&#39;m in the girl gang. We stan me?</p>
 
 
