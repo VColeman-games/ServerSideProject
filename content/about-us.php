b2f656a05a18755b7ed8e9c1b16a8f18bd2732ce
cjmarguin <marguin_claudia@columbusstate.edu>
2019-10-03 20:49:32 +0000

    all pics updated

--- content/about-us.php
+++ content/about-us.php
@@ -5,29 +5,34 @@ Here&#39;s a little about each of us!</p>
 
 <h3>Valencia Coleman</h3>
 
-<img src ="Valencia Coleman.jpg" height="25%" width="25%" alt=" [WE STAN VALENCIA]">
+<img src ="joe.jpg" height="25%" width="25%" alt=" [WE STAN VALENCIA]">
 
-<p> Valencia is a games track queeeeen! Always repin&#39; CSU is the best of ways, whether it&#39;s through
+<p> This is Valencia. Valencia is a games track queeeeen! Always repin&#39; CSU is the best of ways, whether it&#39;s through
 her work effort or her extracurriculars in the Comp Sci department. Mary says that Valencia has not one, but TWO elbows! Valencia is in the girl gang. We stan Valencia. </p>
 
 <br>
 
 <h3>Sharon Deloach </h3>
+<img src ="Nick_Jonas_in_2017.jpg" height="25%" width="25%" alt=" [WE STAN SHARON]">
 
-<p>Sharon is a style ICONNNNN! Always rockin chokers and clothes that I absolutely wish I had the ability to pull-off. On top of that
+<p>This is Sharon. Sharon is a style ICONNNNN! Always rockin chokers and clothes that I absolutely wish I had the ability to pull-off. On top of that
 Sharon is a coding queen! Sharon is in the girl gang. We stan Sharon. </p>
 
 <br>
 
 <h3> Mary Harrell </h3>
 
-<p>Mary is a music feastival fanatic! Of the girl gang, I&#39;ve known Mary the longest (since Data Structures many moons ago).
+<img src ="llcoolj.jpg" height="25%" width="25%" alt=" [WE STAN MARY]">
+
+<p>This is Mary. Mary is a music feastival fanatic! Of the girl gang, I&#39;ve known Mary the longest (since Data Structures many moons ago).
 We (attempt) to work in the Shannon Hall Math/Literature tutor lab as tutors together on. (We&#39;re not the best at algebra,
 but we try and that&#39;s what really matters). Mary is in the girl gang. We stan Mary. </p>
 
 <br>
 
 <h3> Claudia Marguin </h3>
+
+<img src ="kev.jpg" height="25%" width="25%" alt=" [WE STAN VALENCIA]">
 
 <p> Lol. This is me. I love dogs and Minecraft. While I&#39;m also passionate about computer science like the three other amazing
 gal in this group, I&#39;m also a passionate member of greek life here at CSU. I&#39;m in the girl gang. We stan me?</p>
