0f9651adf282b650406bfcc7b2717ea8eeac0700
cjmarguin <marguin_claudia@columbusstate.edu>
2019-10-03 04:51:44 +0000

    css tweaks?

--- content/about-us.php
+++ content/about-us.php
@@ -3,21 +3,21 @@ Here&#39;s a little about each of us!</p>
 
 <br>
 
-<h1>Valencia Coleman</h1>
+<h3>Valencia Coleman</h3>
 
 <p> Valencia is a games track queeeeen! Always repin&#39; CSU is the best of ways, whether it&#39;s through
 her work effort or her extracurriculars in the Comp Sci department. Mary says that Valencia has not one, but TWO elbows! Valencia is in the girl gang. We stan Valencia. </p>
 
 <br>
 
-<h2>Sharon Deloach </h2>
+<h3>Sharon Deloach </h3>
 
 <p>Sharon is a style ICONNNNN! Always rockin chokers and clothes that I absolutely wish I had the ability to pull-off. On top of that
 Sharon is a coding queen! Sharon is in the girl gang. We stan Sharon. </p>
 
 <br>
 
-<h2> Mary Harrell </h2>
+<h3> Mary Harrell </h3>
 
 <p>Mary is a music feastival fanatic! Of the girl gang, I&#39;ve known Mary the longest (since Data Structures many moons ago).
 We (attempt) to work in the Shannon Hall Math/Literature tutor lab as tutors together on. (We&#39;re not the best at algebra,
@@ -25,7 +25,7 @@ but we try and that&#39;s what really matters). Mary is in the girl gang. We sta
 
 <br>
 
-<h2> Claudia Marguin </h2>
+<h3> Claudia Marguin </h3>
 
 <p> Lol. This is me. I love dogs and Minecraft. While I&#39;m also passionate about computer science like the three other amazing
 gal in this group, I&#39;m also a passionate member of greek life here at CSU. I&#39;m in the girl gang. We stan me?</p>
