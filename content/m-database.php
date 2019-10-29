6cf8067f2e8c192a347e4ae94a48b6a2a537def5
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-29 00:10:32 +0000

    att32

--- content/m-database.php
+++ content/m-database.php
@@ -1,8 +1,6 @@
 <div class="row" style="margin-right: 0px; margin-left: 0px;">
 	<div class="mx-auto" style="padding:20px">
-    <div class="center">
     <div> <?php getMovieTitle(); ?> </div>
     <img style="width:auto; height: auto;" src = <?php getMovieImage(); ?>>
-    </div>
     </div>
     </div>
