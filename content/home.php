b0bd584b49e3cd3c2ac894ca9b6f0443daee8549
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-29 19:13:31 +0000

    Testing user input #5

--- content/home.php
+++ content/home.php
@@ -1,33 +1,9 @@
-<script>
-           $(document).ready(function(){
-  $("#submit").click(function(){
-                 //location.reload();
-
-                $.ajax({
-
-                    type: "post",
-                    url: "/includes/function.php",
-                    data: {'userInput': true},
-                    dataType: "HTML",
-                    success: function (data) {
-                        document.getElementById('info').innerHTML = data;
-                    },
-                    error: function (result) {
-                        alert("Error");
-                    }
-                });
-                return false;
-            });
-           });
-        </script>
 
 <div class="center">
 Welcome to the MOOVIES!
-<form>
+<form action= <?php searching(); ?> method = "POST">
 <input type="text" id="myInput"  name = "userInput" placeholder="Search for Movie..">
 <input type="submit" id="submit">
 </form>
-<div id ="info">
 
-</div>
 </div>
