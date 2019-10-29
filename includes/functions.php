eee6f60389f4ef6aafb46e2545502ec2e9b54100
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-29 19:01:39 +0000

    Testing user input #2

--- includes/functions.php
+++ includes/functions.php
@@ -90,6 +90,35 @@ echo '<h5>' . $response["Rated"] . '</h5>';
 echo ' <img style="width:auto; height: auto;" src ='. $response["Poster"]. '>';
 }
 
+function searching()
+{
+    $url = 'http://www.omdbapi.com/?i=tt3896198&apikey=b1892baf';
+/**dont change
+*/
+$handle = curl_init();
+curl_setopt($handle, CURLOPT_URL, $url);
+curl_setopt_array($handle,
+array(
+CURLOPT_URL => $url,
+CURLOPT_RETURNTRANSFER => true
+)
+);
+$output = curl_exec($handle);
+$response = json_decode($output, true);
+curl_close($handle);
+/*dont change
+*/
+echo 'You searched:'.  $_GET["name"];
+
+}
+$userInput = true ;
+if(isset($userInput)){
+    searching();
+}
+
+
+
+
 
 /**
  * Starts everything and displays the template.
