646b6a7fcea15729dc3acdf1f2e5f799e2499932
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-29 22:21:49 +0000

    updated contact us

--- content/contact-us.php
+++ content/contact-us.php
@@ -4,6 +4,12 @@
     height: 93px;
 ">
 <p><b>This is the contact page for our website &#x1F921,&#x1F921,&#x1F921,&#x1F921!!</b></p>
+<p> Emails:<p>
+<b></b>
+<p><b>coleman_valencia@columbusstate.edu</b></p>
+<p><b>harrell_mary@columbusstate.edu</b></p>
+<p><b>deloach_sharon@columbusstate.edu</b></p>
+<p><b>marguin_claudia@columbusstate.edu</b></p>
 <img src ="gotem.jpg" height="25%" width="25%" alt=" gotteeem">
 <div>
 </div>
