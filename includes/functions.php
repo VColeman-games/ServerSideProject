6ac224220056f32f161c04c35be61f8f0ff48e25
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-29 20:56:20 +0000

    trying search #9

--- includes/functions.php
+++ includes/functions.php
@@ -110,15 +110,16 @@ $response = json_decode($output, true);
 curl_close($handle);
 /*dont change
 */
+/*
 foreach($reponse as $key[$value]) {
 echo '<h3>' . $response["Title"] . '</h3>';
 echo '<h4>' . $response["Year"] . '</h4>';
 echo '<h5>' . $response["Rated"] . '</h5>';
-echo ' <img style="width:auto; height: auto;" src ='. $response["Poster"]. '>';
+echo ' <img style="width:auto; height: auto;" src ='. $response["Poster"]. '>';*/
 }
 
 
-}
+
 
 
 
