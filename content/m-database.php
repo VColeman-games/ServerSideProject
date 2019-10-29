a741a151124963cf444a8b6e77b3b952affc0bdb
Mary Harrell <harrell_mary@columbusstate.edu>
2019-10-29 12:01:05 +0000

    search div funtionchg

--- content/m-database.php
+++ content/m-database.php
@@ -1,5 +1,5 @@
 <div> <?php getMovieTitle(); ?> </div>
-<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+<input type="text" id="myInput" onkeyup="getMovieTitle()" placeholder="Search for films..">
 <div class="row" style="margin-right: 0px; margin-left: 0px;">
 	<div class="mx-auto" style="padding:20px">
     <img style="width:auto; height: auto;" src = <?php getMovieImage(); ?>>
