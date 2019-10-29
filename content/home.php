1384f2b342c1592c3192eccb62b7181bf2e47dd4
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-29 20:35:25 +0000

    trying search #4

--- content/home.php
+++ content/home.php
@@ -8,6 +8,6 @@ Welcome to the MOOVIES!
 <input type="text" id="myInput" name="userInput"  placeholder="Search Movies" >
 
 <button > Search</button>
- <div> <?php searching(); ?> </div>
+ <div> <?php getMovie(); ?> </div>
 </div>
 
