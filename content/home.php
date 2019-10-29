4e7eb30728948a282af9b92347498476a77c5bca
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-29 19:35:07 +0000

    Adding black and white stripes

--- content/home.php
+++ content/home.php
@@ -1,5 +1,6 @@
 
 <div class="center">
+<img src="black_white_stripes2_cropped.jpg">
 Welcome to the MOOVIES!
 <input type="text" id="myInput"  name = "userInput" placeholder="Search for Movie..">
 <input type="submit" id="submit">
