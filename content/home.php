27f92c76b156832311925b799c0a81a82183c321
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-29 19:17:28 +0000

    Testing user input #6

--- content/home.php
+++ content/home.php
@@ -1,9 +1,6 @@
 
 <div class="center">
 Welcome to the MOOVIES!
-<form action= <?php searching(); ?> method = "POST">
 <input type="text" id="myInput"  name = "userInput" placeholder="Search for Movie..">
 <input type="submit" id="submit">
-</form>
-
 </div>
