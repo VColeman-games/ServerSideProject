525ad5ce3bf60c9ab0f91863de3e4ba5cefd6454
Claudia "CJ" Marguin <marguin_claudia@columbusstate.edu>
2019-11-13 00:15:57 +0000

    begone thots

--- content/home.php
+++ content/home.php
@@ -1,5 +1,5 @@
 
-<div class="center" > <img src="black_white_stripes2_cropped.jpg" style="width: 136%; height: 93px;"> 
+<div class="center" > 
 Welcome to the MOOVIES!
  <input type="text" id="myInput" name="userInput"  placeholder="Search Movies" > 
 
