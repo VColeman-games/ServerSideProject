835d566b95dfa9540adc74ce5fba58d062658d75
Mary Harrell <harrell_mary@columbusstate.edu>
2019-11-14 05:05:02 +0000

    cowssz

--- content/home.php
+++ content/home.php
@@ -1,8 +1,8 @@
 
 <div class="center" >
 <img src="https://lolsnaps.com/images/upload_pic/135496.jpg" style="
-    width = 50%
-    height = 120px
+    width = %135
+    height = 90px
 ">
 Welcome to the MOOVIES!
 <input type="text" id="myInput" name="userInput"  placeholder="Search Movies" >
