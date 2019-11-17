8ca9b7da26ad76c8072a995a954d585203ffa612
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-11-17 02:21:27 +0000

    ss

--- content/home.php
+++ content/home.php
@@ -1,8 +1,8 @@
 
 <div class="center" >
-Welcome to the MOOVIES!
+Welcome to the MOOVIES! &nbsp;
 <input type="text" id="myInput" name="userInput"  placeholder="Search Movies" >
-
+&nbsp;
 <button type="button" class="btn btn-primary"> Search</button>
  <div> <?php searching(); ?> </div>
 </div>
