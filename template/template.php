a5938cbd0b4bc3bb6bcada67ed94e56e6a0914ee
Mary Harrell <harrell_mary@columbusstate.edu>
2019-11-19 04:55:54 +0000

    wkdfeaohlss

--- template/template.php
+++ template/template.php
@@ -18,8 +18,7 @@
 
     <span class="navbar-toggler-icon"></span>
   </button>
-   <a> <?php site_name(); ?></a>
-
+   
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
          <?php  nav_menu(); ?>
