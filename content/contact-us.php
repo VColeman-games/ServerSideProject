21feed1de559bb0956659b543a4fd347bc7e5676
Mary Harrell <harrell_mary@columbusstate.edu>
2019-11-19 03:38:53 +0000

    j

--- content/contact-us.php
+++ content/contact-us.php
@@ -4,7 +4,7 @@
     <img src= 'disco-ball.svg' width= '10%'/>
     <h1> Contact Us</h1>     
     <p>Hit us up...or drop us a line ...or catch us on the wire ..</p>
-    <i class="fas fa-paper-plane"></i>
+    
   </div>
 </div>
 <div class="borderB">
