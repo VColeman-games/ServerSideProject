1c3ab7e2008dd6df08ec76c2ef6ebf392db68e6a
Mary Harrell <harrell_mary@columbusstate.edu>
2019-11-19 13:42:07 +0000

    nnsslklaal

--- content/m-database.php
+++ content/m-database.php
@@ -1,1 +1,10 @@
+
+<div class="center" >
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <img src= 'film.svg' width= '10%'/>
+    <h1>Find your Flicks</h1>     
+    <p><b><i>where could it be...</i></b></p>
+  </div>
+</div>
 <img src= 'ufo-and-cow.svg' width= '25%'/>
