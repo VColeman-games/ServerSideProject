9f66ddd40aadbe56bf8a94146641f8b95eb5af24
VColeman-games <coleman_valencia@columbusstate.edu>
2019-11-19 07:17:41 +0000

    Putting everything together

--- content/home.php
+++ content/home.php
@@ -7,9 +7,35 @@
   </div>
 </div>
 &nbsp;
-<input type="text" id="myInput" name="userInput"  placeholder="Search Movies" >
+<input type="text" id="userSearching" name="userInput"  placeholder="Search Movies" required>
 &nbsp;
-<button type="button" class="btn btn-primary"> Search</button>
- <div> <?php searching(); ?> </div>
-</div>
+<button id = "searchButton" type="submit" class="btn btn-primary" > Search </button>
 
+
+ <div id="resultDiv">
+ <?php  ?>
+  </div>
+</div>
+<script>
+
+$(document).ready(function(){
+    $("button").click(function(){
+        var searchInput = document.getElementById("userSearching").value;
+        //alert(searchInput);
+
+        $.ajax({
+            type: "POST",
+            url: "/includes/searchFunction.php",
+            data: {'search': document.getElementById("userSearching").value},
+            dataType: 'text',
+            success: function (result) {
+                 $("#resultDiv").html(result);
+                    }
+
+
+        });
+    
+    });
+});
+
+</script>
---
+++ includes/searchFunction.php
@@ -0,0 +1,11 @@
+<?php
+require 'functions.php';
+
+if(isset($_POST['search'])) {
+  $Input = $_POST['search'];
+  echo searching($Input);
+} else {
+  echo "Movie Note Found";
+};
+
+?>
