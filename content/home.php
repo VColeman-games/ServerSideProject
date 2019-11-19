f4a4ff07673243a129c3cab2ac329edebf672e5d
Mary Harrell <harrell_mary@columbusstate.edu>
2019-11-19 14:08:05 +0000

    nnssp

--- content/home.php
+++ content/home.php
@@ -39,3 +39,5 @@ $(document).ready(function(){
 });
 
 </script>
+<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
