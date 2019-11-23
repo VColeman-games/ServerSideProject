16c09329e4abd62c578ccf6aa3f82a27942f00d6
VColeman-games <coleman_valencia@columbusstate.edu>
2019-11-23 09:42:42 +0000

    Edit value #36

--- includes/functions.php
+++ includes/functions.php
@@ -128,11 +128,10 @@ foreach($movieinfo as $key => $value) {
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
         </button>
       </div>';
     echo '<div class="modal-body">'. MovieIMDB($value['imdbID']). '</p></div>';
-    echo '<div class="modal-footer"><button type="button" class="btn btn-primary">Save changes</button> <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>';
+    echo '<div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>';
     
     echo '</div></div></div></div>';
     
