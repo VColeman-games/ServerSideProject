c3f0c254850a470c54dd95ff581d7d83f0221274
VColeman-games <coleman_valencia@columbusstate.edu>
2019-11-23 07:01:49 +0000

    Added movie modal #9

--- includes/functions.php
+++ includes/functions.php
@@ -113,6 +113,7 @@ curl_close($handle);
 /*dont change
 */
 $movieinfo = $response["Search"];
+ echo '<div class ="movie-wrapper">';
 foreach($movieinfo as $key => $value) {
     echo '<h3>' . $value["Title"] . '</h3>';
     echo '<h4>' . $value["Year"] . '</h4>';
@@ -144,7 +145,7 @@ foreach($movieinfo as $key => $value) {
 </div>';
         
 }
-    
+    echo '</div>';
 }
 }
 
