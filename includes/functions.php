b33fd386ba622ce6f206464f1a850b76794d2982
VColeman-games <coleman_valencia@columbusstate.edu>
2019-11-23 08:55:52 +0000

    Edit value #24

--- includes/functions.php
+++ includes/functions.php
@@ -121,19 +121,7 @@ foreach($movieinfo as $key => $value) {
     echo '<h3>' . $value["Title"] . '</h3>';
     echo '<h4>' . $value["Year"] . '</h4>';
     echo '<div ><img style="width:auto; height: auto;" src ='. $value["Poster"]. '></div><br>';
-    echo '<div class="container">';
-    echo '<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" onli>Film Info</button>';
-    echo '<div class="modal fade" id="myModal" role="dialog">';
-    echo ' <div class="modal-dialog">';
-     echo' <div class="modal-content">';
-       echo '<div class="modal-header">';
-          echo '<button type="button" class="close" data-dismiss="modal">&times;</button>';
-        echo '</div>';
-        echo '<div class="modal-body">'. MovieIMDB($value['imdbID']). '</div>';
-        echo '<div class="modal-footer">';
-          echo '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>';
-        echo '</div>';
-      echo '</div></div></div></div></div>';
+    echo '<p">'. MovieIMDB($value['imdbID']). '</p>';
       
 
 
