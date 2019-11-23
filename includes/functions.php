6fe48cd41f1b97401f543cd717271c95ab50bd1e
VColeman-games <coleman_valencia@columbusstate.edu>
2019-11-23 08:31:54 +0000

    Edit value #17

--- includes/functions.php
+++ includes/functions.php
@@ -119,31 +119,23 @@ foreach($movieinfo as $key => $value) {
     echo '<h3>' . $value["Title"] . '</h3>';
     echo '<h4>' . $value["Year"] . '</h4>';
     echo '<div ><img style="width:auto; height: auto;" src ='. $value["Poster"]. '></div><br>';
-    echo '<div class="container">
-  <!-- Trigger the modal with a button -->
-  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" onli>Film Info</button>
-
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-        <div class="modal-body">';
-       echo MovieIMDB($value['imdbID']);
-       echo '</div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
+    echo '<div class="container">';
+    echo '<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" onli>Film Info</button>';
+    echo '<div class="modal fade" id="myModal" role="dialog">';
+    echo ' <div class="modal-dialog">';
+     echo' <div class="modal-content">';
+       echo '<div class="modal-header">';
+          echo '<button type="button" class="close" data-dismiss="modal">&times;</button>';
+        echo '</div>';
+        echo '<div class="modal-body">';
+         MovieIMDB($value['imdbID']);
+       echo '</div>';
+        echo '<div class="modal-footer">';
+          echo '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>';
+        echo '</div>';
+      echo '</div></div></div></div></div>';
       
-    </div>
-  </div>
-  
-</div></div>';
+
 
         
 }
