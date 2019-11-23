909cf70b8802c31f6618691787766fc7b5615450
VColeman-games <coleman_valencia@columbusstate.edu>
2019-11-23 10:32:32 +0000

    Edit value #50

--- includes/functions.php
+++ includes/functions.php
@@ -124,16 +124,17 @@ foreach($movieinfo as $key => $value) {
 echo '<div class="container">
   <!-- Trigger the modal with a button -->
   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" onli>Film Info</button>
-  <!-- Modal -->
-  <div " id="myModal" role="dialog">
+  div class="modal" id="myModal">
     <div class="modal-dialog">
-    
-      <!-- Modal content-->
       <div class="modal-content">
+      
+        <!-- Modal Header -->
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h1 class="modal-title">Modal Heading</h1>
+          <button type="button" class="close" data-dismiss="modal">×</button>
         </div>
         <div class="modal-body">';
+
        MovieIMDB($value['imdbID']);
        echo '</div>
         <div class="modal-footer">
