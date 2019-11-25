8fb3ee0fc227682a7bcb88c14b77473075991966
VColeman-games <coleman_valencia@columbusstate.edu>
2019-11-25 22:57:02 +0000

    Tryign modal ##

--- includes/functions.php
+++ includes/functions.php
@@ -120,9 +120,10 @@ foreach($movieinfo as $key => $value) {
     echo '<div class="grid-item">';
     echo '<h3>' . $value["Title"] . '</h3>';
     echo '<h4>' . $value["Year"] . '</h4>';
-    echo '<img style="width:auto; height: auto; src= '. $value["Poster"] . '></div></div>';
+    echo '<img style="width:auto; height: auto; src= '. $value["Poster"] . '/></div>';
 
 }
+echo '</div>';
 }
 }
 
