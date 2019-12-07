6e515de33db366703aba25fa73a3578558b3fbf4
VColeman-games <coleman_valencia@columbusstate.edu>
2019-12-07 16:18:14 +0000

    trying modal #6

--- includes/functions.php
+++ includes/functions.php
@@ -149,7 +149,6 @@ curl_close($handle);
   
     echo '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Open Modal</button>';
 
-    echo ' <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">';
     echo '  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">';
